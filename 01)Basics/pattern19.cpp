bfe439be72cea5bee7b097ab47445ddc3ff1674a
gomesdisha <dishagomes2005@gmail.com>
2025-06-17 17:39:31 +0530

    q19 spaces modified

--- 01)Basics/pattern19.cpp
+++ 01)Basics/pattern19.cpp
@@ -8,15 +8,15 @@ void print1(int n)
     {
         for(j = n-i; j >= 1; j--)
         {
-            cout << "*";
+            cout << "* ";
         }
         for(j=0;j<2*(i);j++)
         {
-            cout<<" ";
+            cout<<"  ";
         }
          for(j = 1; j <= n-i; j++)
         {
-            cout << "*";
+            cout << "* ";
         }
         cout << endl;
     }
@@ -24,18 +24,46 @@ void print1(int n)
     {
         for(j = 0; j <= i; j++)
         {
-            cout << "*";
+            cout << "* ";
         }
         for(j=0;j<2*(n-i-1);j++)
         {
-            cout<<" ";
+            cout<<"  ";
         }
          for(j = 0; j<= i; j++)
         {
-            cout << "*";
+            cout << "* ";
         }
         cout << endl;
     }
+    //  int iniS = 0;  //better in terms of spacing var, inis
+    //   for(int i=0;i< N;i++){
+    //       for(int j=1;j<=N-i;j++){
+    //           cout<<"*";
+    //       }
+    //       for(int j=0;j<iniS;j++){
+    //           cout<<" ";
+    //       }
+    //       for(int j=1;j<=N-i;j++){
+    //           cout<<"*";
+    //       }
+    //       iniS+=2;
+    //       cout<<endl;
+    //   }
+    //   iniS = 2*N -2;
+    //   for(int i=1;i<=N;i++){
+    //       for(int j=1;j<=i;j++){
+    //           cout<<"*";
+    //       }
+    //       for(int j=0;j<iniS;j++){
+    //           cout<<" ";
+    //       }
+    //       for(int j=1;j<=i;j++){
+    //           cout<<"*";
+    //       }
+    //       iniS-=2;
+    //       cout<<endl;
+    //   }
 }
 
 int main()
