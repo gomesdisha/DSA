31c56080959a0aa0330f55ad0959a16a430cc69e
gomesdisha <dishagomes2005@gmail.com>
2025-06-17 12:51:36 +0530

    q16 faster

--- 01)Basics/pattern16.cpp
+++ 01)Basics/pattern16.cpp
@@ -3,14 +3,22 @@ using namespace std;
 void print1(int n)
 {
     int i,j;
-    char c='A';
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=i;j++)
+    //char c='A';
+    // for(i=1;i<=n;i++)
+    // {
+    //     for(j=1;j<=i;j++)
+    //     {
+    //         cout<<c<<" ";
+    //     }
+    //     c += 1;
+    //     cout<<endl;
+    // }
+     for(i=0;i<n;i++)
+    {    char c='A'+i;
+        for(j=0;j<=i;j++)
         {
             cout<<c<<" ";
         }
-        c += 1;
         cout<<endl;
     }
 }
