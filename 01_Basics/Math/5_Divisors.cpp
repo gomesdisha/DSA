4563aa54101c4194eccd69aebec5bd1d2075692a
gomesdisha <dishagomes2005@gmail.com>
2025-06-20 18:40:48 +0530

    optimised complexity

--- 01_Basics/Math/5_Divisors.cpp
+++ 01_Basics/Math/5_Divisors.cpp
@@ -3,10 +3,16 @@ using namespace std;
 void math(int n)
 {
     cout<<"divisors are : "<<endl;
-    for(int i=1;i<=n;i++)
+    // for(int i=1;i<=n;i++)//time complexxity is o(n)
+    // {
+    //     if(n%i==0)
+    //      cout<<i<<endl;
+    // 
+    //}
+    for(int i=1;i<=sqrt(n);i++)
     {
         if(n%i==0)
-         cout<<i<<endl;
+        cout<<i<<endl<<n/i<<endl;
     }
 }
 int main()
