5579d2e8cc1a2380e716e3350f91d5d5bb1d96d7
gomesdisha <dishagomes2005@gmail.com>
2025-06-22 13:04:38 +0530

    print numbers from 1 to N using recursion,no gv

---
+++ 01_Basics/Recursion/basic_rec_numPrint_1toN.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void rec(int i,int n)
+{
+    if(i>n)
+        return;
+    cout<<i<<endl;
+    rec(i+1,n);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    rec(1,n);
+    return 0;
+}
