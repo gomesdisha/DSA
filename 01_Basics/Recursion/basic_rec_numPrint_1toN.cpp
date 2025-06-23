77c21ed2331738900d82738280552b6ea3d91d04
gomesdisha <dishagomes2005@gmail.com>
2025-06-23 13:16:16 +0530

    1toN and Nto1 dont using BACKTRACKING

--- 01_Basics/Recursion/basic_rec_numPrint_1toN.cpp
+++ 01_Basics/Recursion/basic_rec_numPrint_1toN.cpp
@@ -1,16 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-void rec(int i,int n)
+// void rec(int i,int n) //normal recursion using i
+// {
+//     if(i>n)
+//         return;
+//     cout<<i<<endl;
+//     rec(i+1,n);
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     rec(1,n);
+//     return 0;
+// }
+void rec(int i,int n)  //using backtracking method
 {
-    if(i>n)
+    if(i<1)
         return;
-    cout<<i<<endl;
-    rec(i+1,n);
+    rec(i-1,n);
+     cout<<i<<endl; //prints when backtracking starts
 }
 int main()
 {
     int n;
     cin>>n;
-    rec(1,n);
+    rec(n,n);
     return 0;
 }
--- 01_Basics/Recursion/basic_rec_numPrint_Nto1.cpp
+++ 01_Basics/Recursion/basic_rec_numPrint_Nto1.cpp
@@ -1,17 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-void rec(int i)
+// void rec(int i)
+// {
+//     if(i<=0)
+//         return;
+//     cout<<i<<endl;
+//    // i--;
+//     rec(i-1);
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     rec(n);
+//     return 0;
+// }
+
+void rec(int i,int n) //using backtracking method
 {
-    if(i<=0)
+    if(i>n)
         return;
-    cout<<i<<endl;
    // i--;
-    rec(i-1);
+    rec(i+1,n);
+    cout<<i<<endl;
 }
 int main()
 {
     int n;
     cin>>n;
-    rec(n);
+    rec(1,n);
     return 0;
 }
