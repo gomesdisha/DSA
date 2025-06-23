069db1ae91e341835934d3bc8593ff6723473780
gomesdisha <dishagomes2005@gmail.com>
2025-06-23 13:48:41 +0530

    now added functional way too ,initially was in parameterised recursion

--- 01_Basics/Recursion/basic_rec_FactOfN_numbers.cpp
+++ 01_Basics/Recursion/basic_rec_FactOfN_numbers.cpp
@@ -1,18 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-void facto(int n,int i,int fact)
+// void facto(int n,int i,int fact) //PARAMETERISED WAY
+// {
+//     if(i>n)
+//         {cout<<"factorial of "<<n<<" is "<<fact;
+//         return;}
+//     //i++;
+//     //fact(n,i,fact);
+//     facto(n,i+1,fact*i);
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     facto(n,1,1);
+//     return 0;
+// }
+
+int facto(int n)//FUNCTIONAL WAY
 {
-    if(i>n)
-        {cout<<"factorial of "<<n<<" is "<<fact;
-        return;}
-    //i++;
-    //fact(n,i,fact);
-    facto(n,i+1,fact*i);
+   if(n==0)
+        return 1;
+    return n*facto(n-1);
+   
 }
 int main()
 {
     int n;
     cin>>n;
-    facto(n,1,1);
+    cout<<"factorial of "<<n<<" is "<<facto(n);
     return 0;
 }
--- 01_Basics/Recursion/basic_rec_SumOfN_numbers.cpp
+++ 01_Basics/Recursion/basic_rec_SumOfN_numbers.cpp
@@ -1,19 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-void rec(int n,int i,int sum)
+// void rec(int n,int i,int sum) //PARAMETERISED WAY
+// {
+//     if(i>n)
+//     {   
+//         cout<<"sum of "<<n<<" numbers is "<<sum<<endl;;
+//         return;
+//     }
+//     //sum+=i;
+//     rec(n,i+1,sum+i);  
+// }
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     rec(n,1,0);
+//     return 0;
+// }
+int rec(int n) //FUNCTIONAL WAY
 {
-    if(i>n)
-    {   
-        cout<<"sum of "<<n<<" numbers is "<<sum<<endl;;
-        return;
-    }
+    if(n==0)
+        return 0;
     //sum+=i;
-    rec(n,i+1,sum+i);  
+    return n + rec(n-1);
 }
 int main()
 {
     int n;
     cin>>n;
-    rec(n,1,0);
+   // rec(n,1,0);
+   cout<<"sum of "<<n<<" numbers is "<<rec(n);
     return 0;
 }
